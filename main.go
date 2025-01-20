453dae865c0e9c9d7ac381e6f661b05690084bda
JonHunt1995 <jnthnhunt495@gmail.com>
2025-01-19 22:31:39 -0600

    Added commands help and exit to REPL

--- main.go
+++ main.go
@@ -18,9 +18,41 @@ func cleanInput(text string) []string {
 	return sanitizedWords
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:/n/n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	r := os.Stdin
 	s := bufio.NewScanner(r)
+	supportedCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		}, "help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	for {
 		fmt.Print("Pokedex > ")
 		if ok := s.Scan(); !ok {
@@ -33,6 +65,14 @@ func main() {
 			fmt.Println("Input is empty, please try again")
 			continue
 		}
-		fmt.Printf("Your command was: %v\n", words[0])
+		commandName := words[0]
+
+		command, ok := supportedCommands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
+
 	}
 }
